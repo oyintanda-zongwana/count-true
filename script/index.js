46e2fbf609b77cc6ba340b40f83ae6ec81892c60
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-06-04 16:39:13 +0200

    done

--- script/index.js
+++ script/index.js
@@ -1,7 +1,7 @@
 function countTrueValues() {
     let booleanArray = [true, false, true, true, false, true, false, false, true];
-
+    //to check which values are true
     let trueCount = booleanArray.filter(value => value === true).length;
-
+    //so the result will be shown
     document.getElementById('result').innerHTML = "<p>Number of true values: " + trueCount + "</p>";
 }
